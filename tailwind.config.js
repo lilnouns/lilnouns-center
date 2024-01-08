978673958a58411746c432d0c5b55a0f1048bfea
Milad Nekofar <milad@nekofar.com>
2024-01-08 11:56:51 +0330

    refactor: move source codes to the `src` folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
   mode: 'jit',
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
